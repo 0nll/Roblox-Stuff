729916b1cf51e3d12659032dd482bd5e7051d1ef
Oni <101807511+0nll@users.noreply.github.com>
2022-10-14 15:57:45 +0200

    v0.1

---
+++ Newbie Render Distance.lua
@@ -0,0 +1,64 @@
+-- Settings --
+local RenderDistance = 100
+
+
+
+
+
+
+
+
+
+
+
+-- DONT TOUCH --
+
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspacee = game:GetService("Workspace")
+local RunService = game:GetService("RunService")
+local RenderCache = Instance.new("Folder")
+RenderCache.Parent = ReplicatedStorage
+RenderCache.Name = "RenderCache"
+local LocalPlayer =  Players.LocalPlayer
+local Charecter = script.Parent
+local HumanoidRootPart = Charecter:WaitForChild("HumanoidRootPart")
+local Render = Workspacee:WaitForChild("Render")
+local Parts = {}
+
+function Scan()
+    for _, Object in next, Render:GetChildren() do
+			table.insert(Parts, Object)
+			Object.Parent = RenderCache
+		end
+	end
+
+function GetPart (Object)
+		if(Object:IsA("BasePart"))then
+			return Object
+		else 
+			for _, Obj in next, Object:GetChildren() do
+			return GetPart(Obj)
+		end
+	end
+	return nil
+end
+
+
+function Update()
+	for _, v in next, Parts do
+		local Part = GetPart(v)
+		if (Part) then
+			local Distance = (Part.CFrame.p - HumanoidRootPart.CFrame.p).Magnitude
+			Distance = math.floor(Distance + 0.5)
+			if (Distance <= RenderDistance) then 
+				v.Parent = Render
+			else
+				v.Parent = RenderCache
+			end
+		end
+	end
+end
+Scan()
+	RunService:BindToRenderStep("RenderSys", 1, Update)
